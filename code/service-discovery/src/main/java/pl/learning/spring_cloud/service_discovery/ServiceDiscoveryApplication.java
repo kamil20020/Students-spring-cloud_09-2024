2259bfb2f3f568fbeaba6c81b7f86982ae0d5962
kdywan_254049 <idywan_254049@e-science.pl>
2024-09-10 19:05:08 +0200

    Added eureka service discovery

--- code/service-discovery/src/main/java/pl/learning/spring_cloud/service_discovery/ServiceDiscoveryApplication.java
+++ code/service-discovery/src/main/java/pl/learning/spring_cloud/service_discovery/ServiceDiscoveryApplication.java
@@ -2,12 +2,13 @@ package pl.learning.spring_cloud.service_discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceDiscoveryApplication.class, args);
 	}
-
 }
