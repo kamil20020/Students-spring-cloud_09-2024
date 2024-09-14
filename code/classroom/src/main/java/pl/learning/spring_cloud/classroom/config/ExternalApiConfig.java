a631dd65df17bab0a4049f51765e137ba612c7aa
kdywan_254049 <idywan_254049@e-science.pl>
2024-09-15 00:36:15 +0200

    Added api gateway

--- code/classroom/src/main/java/pl/learning/spring_cloud/classroom/config/ExternalApiConfig.java
+++ code/classroom/src/main/java/pl/learning/spring_cloud/classroom/config/ExternalApiConfig.java
@@ -1,5 +1,6 @@
 package pl.learning.spring_cloud.classroom.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
@@ -7,12 +8,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@LoadBalancerClient(name = "student")
 public class ExternalApiConfig {
 
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.build();
     }
 }
